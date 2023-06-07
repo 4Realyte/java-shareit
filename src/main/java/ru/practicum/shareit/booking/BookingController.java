0067aafced8a40fbacb583958db31661f0844c14
IvanB <ibaletov@yandex.ru>
2023-06-07 20:18:24 +0300

    feature: paging bookings

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,18 +1,23 @@
 package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
+import ru.practicum.shareit.booking.dto.GetBookingRequest;
 import ru.practicum.shareit.booking.service.BookingService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor
+@Validated
 public class BookingController {
     private final BookingService bookingServiceImpl;
 
@@ -35,13 +40,17 @@ public class BookingController {
 
     @GetMapping
     public List<BookingResponseDto> getAllUserBookings(@RequestParam(defaultValue = "ALL") State state,
-                                                       @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
-        return bookingServiceImpl.getAllUserBookings(state, userId, false);
+                                                       @RequestHeader("X-Sharer-User-Id") @NotNull Long userId,
+                                                       @RequestParam(required = false, defaultValue = "0") @PositiveOrZero int from,
+                                                       @RequestParam(required = false, defaultValue = "10") @Positive int size) {
+        return bookingServiceImpl.getAllUserBookings(GetBookingRequest.of(state, userId, false, from, size));
     }
 
     @GetMapping("/owner")
     public List<BookingResponseDto> getAllUserItemBookings(@RequestParam(defaultValue = "ALL") State state,
-                                                           @RequestHeader("X-Sharer-User-Id") @NotNull Long userId) {
-        return bookingServiceImpl.getAllUserBookings(state, userId, true);
+                                                           @RequestHeader("X-Sharer-User-Id") @NotNull Long userId,
+                                                           @RequestParam(required = false, defaultValue = "0") @PositiveOrZero int from,
+                                                           @RequestParam(required = false, defaultValue = "10") @Positive int size) {
+        return bookingServiceImpl.getAllUserBookings(GetBookingRequest.of(state, userId, true, from, size));
     }
 }
